9a43fa60e0ab43cfb61d713fac37b4f3a884756f
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:45:20 +0500

    docs: code comments qo'shildi

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { Sequelize, DataTypes, where } = require("sequelize")
 // database testlari qo'shildi
 // real-time notifications implementatsiya qilindi
 
+// database testlari qo'shildi
 // type error tuzatildi
 // kod formatlash va tozalash
 const app = express()
