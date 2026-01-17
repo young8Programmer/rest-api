7c52ba3d2af4c78fbe3e7a25fcf085599dc6a5af
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:45:54 +0500

    perf: image optimization qo'shildi

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // installation qo'llanmasi yaratildi
 // component testlari yaratildi
 // kod uslubini yaxshilash
+// installation qo'llanmasi yaratildi
 // API endpoint testlari qo'shildi
 // bundle size optimallashtirildi
 // unit testlar qo'shildi
