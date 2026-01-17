42cba2ff96118aed66a321d94b7c376cd7ab0007
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:45:52 +0500

    chore: build konfiguratsiyasi sozlandi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // kod uslubini yaxshilash
 // bundle size optimallashtirildi
 // unit testlar qo'shildi
+// code comments qo'shildi
 // bundle size optimallashtirildi
 // API endpoint testlari qo'shildi
 // package.json yangilandi
