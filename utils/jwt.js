71e3fe9e993661ffbd04f55f61dd6404b92855c3
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:47:12 +0500

    style: kod formatlash va indentatsiya

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,7 @@
 // routing muammosi hal qilindi
 // kod strukturasini yaxshilash
 // package.json yangilandi
+// bundle size optimallashtirildi
 // CORS xatosi tuzatildi
 // admin dashboard yaratildi
 // admin dashboard yaratildi
