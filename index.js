ea338cfc30fe08c61a0802a03b1e42c8dfaa2e7b
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:45:50 +0500

    style: kod uslubini yaxshilash

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // installation qo'llanmasi yaratildi
 // component testlari yaratildi
 // kod uslubini yaxshilash
+// bundle size optimallashtirildi
 // unit testlar qo'shildi
 // error handling yaxshilandi
 // component testlari yaratildi
