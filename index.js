9a48c59def6557599e849653aaa5f27921ceb41e
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:45:50 +0500

    feat: user authentication qo'shildi

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 // kod uslubini yaxshilash
 // bundle size optimallashtirildi
 // unit testlar qo'shildi
+// package.json yangilandi
 // error handling yaxshilandi
 // component testlari yaratildi
 // installation qo'llanmasi yaratildi
