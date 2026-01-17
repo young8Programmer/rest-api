4611664ed82c29403d74aa32ec37e761519973b2
young8Programmer <valireyimbergenov79@gmail.com>
2026-01-18 00:47:11 +0500

    chore: environment variables sozlandi

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,6 +2,7 @@
 // package.json yangilandi
 // CORS xatosi tuzatildi
 // admin dashboard yaratildi
+// admin dashboard yaratildi
 // integration testlar yaratildi
 // database querylarni optimallashtirish
 const jwt = require("jwt")
